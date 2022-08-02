af945e4d1947d2b4b3ae7014a33d964e2780ad76
AnhNPT <anhnpt2000@gmail.com>
2022-08-02 11:24:18 +0700

    add script

--- pages/index.js
+++ pages/index.js
@@ -149,22 +149,8 @@ export default function Home() {
       {/* Footer */}
       <Footer/>
 
+      <script src='./home_script.js' async />
+
     </div>
   )
-}
-
-
-if (typeof window !== "undefined") {
-  var newContainer = document.querySelectorAll('.newCtn');
-  for (let i = 0; i < newContainer.length; i++) {
-    newContainer[i].addEventListener('mouseover', function () {
-      document.getElementById('title1').style = 'color: red';
-    })
-    newContainer[i].addEventListener('mouseout', function () {
-      document.getElementById('title1').style = 'color: #5168b5';
-    })
-  };
-  document.getElementById('scrollBtn').addEventListener('click', function () {
-    document.getElementById('welcomeSection').scrollIntoView({ behavior: 'smooth' });
-  })
 }
